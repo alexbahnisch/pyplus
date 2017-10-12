fbc8d78559d8ddf3f303493fa5294ac68fecf6be
Alex Bahnisch <alexbahnisch@gmail.com>
2017-10-12 21:41:34 +1000

    Releasing next patch

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if any(arg in argv for arg in ["sdist", "bdist_wheel"]):
 
 setup(
     name="pyplus",
-    version="0.1.4.dev0",
+    version="0.1.4",
     description="A library containing a collection of python extensions.",
     long_description=long_description,
     url="https://github.com/alexbahnisch/pyplus",
