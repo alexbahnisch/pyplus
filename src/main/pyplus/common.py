4019dd51a660ad7365545835d8d1598887a39cdd
Alex Bahnisch <alexbahnisch@gmail.com>
2018-06-28 23:08:58 +1000

    Added docstrings to common.py

--- src/main/pyplus/common.py
+++ src/main/pyplus/common.py
@@ -1,93 +1,113 @@
 """
-A collection of common helper functions to check if an object has characteristics of other objects.
+A collection of common helper functions.
 """
 from decimal import Decimal as _Decimal
 from os import name as _name
 from pathlib import Path as _Path
 
 
-def isintlike(obj):
+def isintlike(value):
     """
-    Checks if an object can be converted to an integer
+    Checks if an object can be converted to an integer.
+    @param value: {object}
+    @return {bool}
     """
     try:
-        int(obj)
+        int(value)
         return True
     except (TypeError, ValueError):
         return False
 
 
-def isiterable(obj, include_strings=True):
+def isiterable(value, include_strings=True):
     """
-    Checks if an object is iterable
+    Checks if an object is iterable.
+    @param value: {object}
+    @param include_strings: {bool} Include strings as an iterable type.
+    @return {bool}
     """
     if include_strings:
-        return hasattr(obj, "__iter__") or isinstance(obj, str)
+        return hasattr(value, "__iter__")
     else:
-        return not isinstance(obj, str) and hasattr(obj, "__iter__")
+        return not isinstance(value, str) and hasattr(value, "__iter__")
 
 
-def islistlike(obj):
+def islistlike(value):
     """
-    Checks if an object has tuple like properties, i.e has a length, is iterable, can get and set items
+    Checks if an object has list like properties, i.e has a length, is iterable, can get and set items.
+    @param value: {object}
+    @return {bool}
     """
-    return hasattr(obj, "__len__") and hasattr(obj, "__getitem__") and hasattr(obj, "__setitem__") and isiterable(obj)
+    return hasattr(value, "__len__") and hasattr(value, "__getitem__") and hasattr(value, "__setitem__") and isiterable(value)
 
 
-def isnumber(obj):
+def isnumber(value):
     """
-    Checks if an object is a number, excludes booleans
+    Checks if an object is a number, excludes booleans.
+    @param value: {object}
+    @return {bool}
     """
-    return isinstance(obj, (_Decimal, float, int)) and not isinstance(obj, bool)
+    return isinstance(value, (_Decimal, float, int)) and not isinstance(value, bool)
 
 
-def ispair(obj):
+def ispair(value):
     """
-    Checks if an object is a pair, i.e is iterable and has a length of 2
+    Checks if an object is a pair, i.e is iterable and has a length of 2.
+    @param value: {object}
+    @return {bool}
     """
-    return hasattr(obj, "__len__") and len(obj) == 2 and isiterable(obj)
+    return hasattr(value, "__len__") and len(value) == 2 and isiterable(value)
 
 
-def ismappable(obj):
+def ismappable(value):
     """
-    Checks if an object is mappable, i.e can be passed into a 'dict' constructor
+    Checks if an object is mappable, i.e can be passed into a 'dict' constructor.
+    @param value: {object}
+    @return {bool}
     """
-    return isinstance(obj, dict) or (isiterable(obj) and all(ispair(item) for item in obj))
+    return isinstance(value, dict) or (isiterable(value) and all(ispair(item) for item in value))
 
 
-def ispathlike(obj):
+def ispathlike(value):
     """
-    Checks if an object has path like properties, i.e. is an instance of a string or pathlib.Path
+    Checks if an object has path like properties, i.e. is an instance of a string or pathlib.Path.
+    @param value: {object}
+    @return {bool}
     """
-    return isinstance(obj, (bytes, str, _Path))
+    return isinstance(value, (bytes, str, _Path))
 
 
-def issequence(obj):
+def issequence(value):
     """
-    Checks if an object has a length and is iterable
+    Checks if an object has a length and is iterable.
+    @param value: {object}
+    @return {bool}
     """
-    return hasattr(obj, "__len__") and isiterable(obj)
+    return hasattr(value, "__len__") and isiterable(value)
 
 
-def istuplike(obj):
+def istuplike(value):
     """
-    Checks if an object has tuple like properties, i.e has a length, is iterable and can get items
+    Checks if an object has tuple like properties, i.e has a length, is iterable and can get items.
+    @param value: {object}
+    @return {bool}
     """
-    return hasattr(obj, "__len__") and hasattr(obj, "__getitem__") and isiterable(obj)
+    return hasattr(value, "__len__") and hasattr(value, "__getitem__") and isiterable(value)
 
 
 def iswindows():
     """
-    Checks if the current operating system is windows
+    Checks if the current operating system is windows.
+    @return {bool}
     """
     return _name == "nt"
 
 
-def iterable(obj):
+def iterable(value):
     """
     @deprecated will be remove next minor
     """
-    if isiterable(obj):
-        return obj
+    if isiterable(value):
+        return value
     else:
-        return [obj]
+        return [value]
